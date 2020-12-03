b7e6b83e4c24d40f625dc6ab249c3336d4d93626
Zach Yanez <zachyanez@gmail.com>
2020-12-03 15:07:19 -0600

    commiting the index.js error

--- server.js
+++ server.js
@@ -16,8 +16,8 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Workout", {
 });
 
 
-app.use(require("./routes/api.js"))
-// require("./routes/api-html.js")(app);
+app.use(require("./routes/api.js")(app));
+app.use(require("./routes/html.js")(app));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
